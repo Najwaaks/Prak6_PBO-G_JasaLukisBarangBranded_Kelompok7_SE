f82f0199c86b9b0e91e41ea6d7abd9276a5610ac
Najwa Ks <najwaa.ks06@gmail.com>
2025-11-11 14:12:01 +0700

    class JasaLukis, atribut, constructor, dan main()

---
+++ inner_class/src/Main.java
@@ -0,0 +1,11 @@
+package inner_class.src;
+import inner_class.src.inner_class.JasaLukis;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== JASA LUKIS BARANG BRANDED ===");
+        
+        JasaLukis jasa1 = new JasaLukis("Lukis Sepatu", "Lukisan custom dengan cat tahan air", 150000);
+        JasaLukis jasa2 = new JasaLukis("Lukis Tas", "Lukisan custom dengan cat tahan air", 200000);
+    }
+}
---
+++ inner_class/src/inner_class/JasaLukis.java
@@ -0,0 +1,15 @@
+package inner_class.src.inner_class;
+
+public class JasaLukis {
+    // atribut
+    String namaJasa; 
+    String deskripsi;
+    double hargaPerBarang;
+
+    // constructor
+    public JasaLukis(String namaJasa, String deskripsi, double hargaPerBarang) {
+        this.namaJasa = namaJasa;
+        this.deskripsi = deskripsi;
+        this.hargaPerBarang = hargaPerBarang;
+    }
+}
